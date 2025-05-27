7c39f289b51b7d2b879283cd3dd3e55ec4d6464f
Deven Nathanael <devennatval@gmail.com>
2025-05-27 23:41:11 +0700

    Initial Commit

---
+++ CoinTrackr/CoinTrackrApp.swift
@@ -0,0 +1,32 @@
+//
+//  CoinTrackrApp.swift
+//  CoinTrackr
+//
+//  Created by Deven Nathanael on 27/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CoinTrackrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CoinTrackrTests/CoinTrackrTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoinTrackrTests.swift
+//  CoinTrackrTests
+//
+//  Created by Deven Nathanael on 27/05/25.
+//
+
+import Testing
+
+struct CoinTrackrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
